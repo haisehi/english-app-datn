433de95adc3b6c0811abeb82bbac4bd783980813
haisehi <88788629+haisehi@users.noreply.github.com>
2025-12-03 23:48:56 +0700

    fix api 2

--- lib/constrants/app_constrants.dart
+++ lib/constrants/app_constrants.dart
@@ -1,7 +1,7 @@
 const String apiUrl = 'http://192.168.1.204:8080/api/v1';
 
 const String tokenTest = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJob2FuZ21pbmhAZ21haWwuY29tIiwiaWF0IjoxNzM0NzA1NjMyLCJleHAiOjE3MzQ3OTIwMzJ9.i7iAeHZwBMp_2n9Nd_gxnp38HVvEjNVrIcVXf_h6kTw";
-// const GEMINI_API_KEY = "AIzaSyDnZ4d21xPDCtqARItfGuovMUSry-pVZ_4";
+const GEMINI_API_KEY = "";
 
 // set JAVA_HOME=C:\Program Files\Java\jdk-17
 // set PATH=%JAVA_HOME%\bin;%C:\Program Files\Java\jdk-17\bin%
